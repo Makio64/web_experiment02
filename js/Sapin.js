992ccaf66eafd245c7ac6ff210433ac61c227d11
Makio64 <davronai@gmail.com>
2012-12-02 23:34:46 +0000

    version alpha

--- js/Sapin.js
+++ js/Sapin.js
@@ -2,7 +2,8 @@
 * Makio - @Makio64 - www.makiopolis.com
 */
 
-var Sapin = function(scene){
+var Sapin = function(scene)
+{
 	scope = this;
 	scope.scene = scene;
 	scope.lines = [];
@@ -10,41 +11,85 @@ var Sapin = function(scene){
 	scope.material = new THREE.LineBasicMaterial({
         color: 0xFFFFFF,
     });
+    scope.vx = 0;
+    scope.ax = 0;
+    scope.speed = 0;
 
     var vectors = [{x:0,y:0},{x:1,y:0},{x:1,y:3},{x:4,y:3},{x:2,y:6},{x:3,y:6},{x:1,y:9},{x:2,y:9},{x:0,y:13}];
 
-    for (var i = 0; i < vectors.length; i++) {
-    	scope.geometry.vertices.push(new THREE.Vector3(vectors[i].y,0,vectors[i].x));
-    };
+   //  for (var i = 0; i < vectors.length; i++) {
+   //  	scope.geometry.vertices.push(new THREE.Vector3(vectors[i].y,0,vectors[i].x));
+   //  };
 
-   for (var i = vectors.length - 2; i >= 0; i--) {
-    	scope.geometry.vertices.push(new THREE.Vector3(vectors[i].y,0,-vectors[i].x));
-    };
+   // for (var i = vectors.length - 2; i >= 0; i--) {
+   //  	scope.geometry.vertices.push(new THREE.Vector3(vectors[i].y,0,-vectors[i].x));
+   //  };
 
-    var line = new THREE.Line(scope.geometry, scope.material);
-    //scene.add( line );
-    scope.rotation = line.rotation;
+   //  var line = new THREE.Line(scope.geometry, scope.material);
+    // scope.rotation = line.rotation;
 
-    particlesGeo = new THREE.Geometry();
-
-    var materials = [];
-    var sprite = THREE.ImageUtils.loadTexture( "img/particle.png" );
+    scope.particlesGeo = new THREE.Geometry();
+    scope.values = [];
+    
+    var sprite = THREE.ImageUtils.loadTexture( "img/circle.png" );
 
     for (var i = 1; i < vectors.length; i++) {
         var start = vectors[i-1]; 
         var dest = vectors[i];
-        var vertex = new THREE.Vector3();
-        for (var j = 0; j < 5; j++) {
-            vertex.x = dest.x*i/5 - start.x;
-            vertex.y = dest.y*i/5 - start.y;
-            vertex.z = dest.z*i/5 - start.z;
-            particlesGeo.vertices.push( vertex );
+        var division = 100;
+        for (var j = 0; j < division; j++) {
+            var vertex = new THREE.Vector3();
+            vertex.x = start.x + (dest.x-start.x)*j/division;
+            scope.values.push({coeff:vertex.x,x:0});
+            vertex.y = start.y + (dest.y-start.y)*j/division;
+            scope.particlesGeo.vertices.push( vertex );
+            
+            vertex = new THREE.Vector3();
+            vertex.x = -(start.x + (dest.x-start.x)*j/division);
+            scope.values.push({coeff:vertex.x,x:Math.PI});
+            vertex.y = start.y + (dest.y-start.y)*j/division;
+            scope.particlesGeo.vertices.push( vertex );
         };
     }
 
-    material = new THREE.ParticleBasicMaterial( { size: 10, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent : true } );
-    particleSystem = new THREE.ParticleSystem(particlesGeo, material);
+    material = new THREE.ParticleBasicMaterial( { size: 1, map: sprite, blending:THREE.AdditiveBlending, transparent : true } );
+    particleSystem = new THREE.ParticleSystem(scope.particlesGeo, material);
+    
     scene.add( particleSystem );
+
+    scope.update = function(scroll) 
+    {    
+        if(scroll) {
+            scope.ax += Math.PI/40;
+        } else {
+            scope.ax -= Math.PI/40; 
+        }
+        scope.vx += scope.ax;
+        scope.speed += scope.vx;
+        var s = scope.speed;
+        if(scope.speed=0)
+            return;
+        var coeffX = 2;
+        var coeffZ = 2;
+        var degreeMax = 40;
+ 
+        var basevx = tmpvx = Math.PI*degreeMax/180;
+        var p;
+        var v;
+        var deceleration = basevx/4000;
+        var l = scope.particlesGeo.vertices.length;
+        for (var i = 0; i < l; i++) {
+            if(tmpvx<0)
+                return;
+            p = scope.particlesGeo.vertices[i];
+            v = scope.values[i];
+            v.x += tmpvx;
+            p.x = Math.cos(v.x)*v.coeff; 
+            p.z = Math.sin(v.x)*v.coeff; 
+            tmpvx -= deceleration;
+        };
+        scope.particlesGeo.verticesNeedUpdate = true;
+    }
 }
 
 Sapin.prototype.constructor = Sapin;
--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ windowHalfX = $(window).width()/2,
 windowHalfY = $(window).height()/2,
 isWebGL = Detector.webgl,
 tanFOV, windowHeight,
-stats, container, camera, scene, renderer, composer,
-activatePlatform = 0;
+stats, container, camera, scene, renderer, composer, analyser,
+state = 0;
 
 
 $(document).ready(function() 
@@ -21,23 +21,42 @@ function initScene(){
 	document.body.appendChild(container);
 
 	//same camera for all
-	camera = new THREE.PerspectiveCamera( 25, $(window).width() / $(window).height(), 1, 10000 );
-	tanFOV = Math.tan( ( ( Math.PI / 180 ) * camera.fov / 2 ) );
+	camera = new THREE.PerspectiveCamera( 25, $(window).width() / $(window).height(), 1, 500 );
 	windowHeight =  $(window).height();
 
 	camera.aspect = $(window).width() / Math.floor($(window).height());
-    camera.fov = ( 360 / Math.PI ) * Math.atan( tanFOV * ( $(window).height() / windowHeight ) );
 	camera.position.set(0, 15, 45);
     camera.lookAt(new THREE.Vector3(0,6.8,0));
 	camera.updateProjectionMatrix();
 	
 	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2( 0x000104, 0.0000675 );
+
 	scene.add(camera);
 	
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({clearColor: 0xFFFFFF, clearAlpha: 0, antialias: false});
 	renderer.setSize( $(window).width(), $(window).height());
+	renderer.setClearColor( 0xFFFFFF, 1 );
+	renderer.autoClear = false;
+	renderer.sort = false;
 	container.appendChild( renderer.domElement );
 	
+	// postprocessing
+	var renderModel = new THREE.RenderPass( scene, camera );
+	var effectBloom = new THREE.BloomPass( 0.95 );
+	var effectFilm = new THREE.FilmPass( 1, 1, 1448, false );
+	effectFocus = new THREE.ShaderPass( THREE.FocusShader );
+	effectFocus.uniforms[ "screenWidth" ].value = screenWidth;
+	effectFocus.uniforms[ "screenHeight" ].value = screenHeight;
+	effectFocus.renderToScreen = true;
+
+	composer = new THREE.EffectComposer( renderer );
+
+	composer.addPass( renderModel );
+	composer.addPass( effectBloom );
+	composer.addPass( effectFilm );
+	composer.addPass( effectFocus );
+
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
 	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
@@ -51,33 +70,89 @@ function initScene(){
 		renderer.setSize( $(window).width(), $(window).height());
 		$(container).height( Math.floor($(window).height()) );
 		camera.aspect = $(window).width() / Math.floor($(window).height());
-		camera.lookAt(new THREE.Vector3(0,0,0));
-		// adjust the FOV
-		camera.fov = ( 360 / Math.PI ) * Math.atan( tanFOV * ( $(window).height() / windowHeight ) );
+		camera.lookAt(new THREE.Vector3(0,6.8,0));
+		
 		camera.updateProjectionMatrix();
+		composer.reset( new THREE.WebGLRenderTarget( screenWidth, screenHeight ) );
 	});
 }
 
-var sapins = [];
-var state = 'line';
-var	ax, vx;
+var sapin, star;
 
 function init() {
-	for (var i = 0; i < 36; i++) {
-		var s = new Sapin(scene);	
-		s.rotation.z = Math.PI/2;
-		s.rotation.y = Math.PI*2*i/36;
-		sapins[i] = s;
+
+	var h,  cube, angle, radius;
+	
+	cube = new THREE.Mesh(
+   		new THREE.CubeGeometry( 40, 1, 40, 1, 1, 1 ),
+    	new THREE.MeshBasicMaterial( {wireframe:true} )
+	);
+	cube.position.y = -1;
+	// scene.add(cube);
+
+	for (var i = 0; i < 10; i++) {
+		h = Math.random()*2+.5;
+		cube = new THREE.Mesh(
+	   		new THREE.CubeGeometry( Math.random()*3+1, h, Math.random()*3+1, 1, 1, 1 ),
+	    	new THREE.MeshBasicMaterial( {wireframe:true, color :Math.random()*0xFFFFFF} )
+		);
+		cube.position.y = h/2-1;
+		cube.rotation.y = Math.PI*Math.random();
+		radius = Math.random()*12+7;
+		angle = (i/10*Math.PI*2)+Math.PI/8*Math.random();
+		cube.position.x = Math.cos(angle)*radius;
+		cube.position.z = Math.sin(angle)*radius;
+		scene.add(cube);
 	}
+	sapin = new Sapin(scene);
+	star = new Star(scene);	
+
+	var audio = new Audio();
+	audio.addEventListener('canplay', function() {
+  		setTimeout(function() {  
+  			star.start();
+  			setTimeout(function() { 
+  				$('body').css('cursor','pointer'); 
+  				$(window).click(function(e){
+	  				$('body').css('cursor','auto');
+	  				if(state==1){
+	  					return;
+	  				}
+	  				setTimeout(function() { state = 1; },4000);
+	  				star.randomize(139,93);
+	    			audio.play();
+	  			})
+  			},1000);
+  		}, 1200);
+    }, false);
+
+	audio.src = 'sfx/music.mp3';
+	audio.controls = true;
+	audio.autoplay = false;
+	document.body.appendChild(audio);
+	var context = new webkitAudioContext();
+	analyser = context.createAnalyser();
+
+	var source = context.createMediaElementSource(audio);
+	source.connect(analyser);
+	analyser.connect(context.destination);
 }
 
 function animate() {
-	for (var i = 0; i < sapins.length; i++) {
-		sapins[i].rotation.y += Math.PI/80;
-	};
+	console.log(analyser);
+	var coeff = 0.05;
+	camera.position.y += ((mouseY/screenHeight)*50+45-camera.position.y)*coeff;
+	camera.position.z += ((mouseY/screenHeight)*60+105-camera.position.z)*coeff;//45
+	camera.position.x += ((mouseX/screenWidth)*60-camera.position.x)*coeff;
+	camera.lookAt(new THREE.Vector3(0,6.8,0));
+		
 	requestAnimationFrame( animate );
 	stats.update();
-	renderer.render(scene, camera);
+	if(state==1){
+		sapin.update((mouseX>screenWidth/2));
+	}
+	star.update();
+	composer.render( 0.00001 );
 }
 
 function onDocumentMouseMove(event) {
